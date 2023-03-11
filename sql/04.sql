de757f795c5f400339ef379151229d663286bdf0
Sybel Francois <123044356+SybelFrancois@users.noreply.github.com>
2023-03-11 13:51:37 -0800

    Update 04.sql

--- sql/04.sql
+++ sql/04.sql
@@ -1,8 +1,10 @@
 /*
- * Select the titles of all films that the customer with customer_id=1 has rented more than 1 time.
+ * Select the titles of all films that the customer with
+ customer_id=1 has rented more than 1 time.
  *
  * HINT:
- * It's possible to solve this problem both with and without subqueries.
+ * It's possible to solve this problem both wi
+ th and without subqueries.
  */
 
 select title from film join inventory using (film_id) 
